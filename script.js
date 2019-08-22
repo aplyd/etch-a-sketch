dd89b3343bdaad059fa6cd9afb00dba0276afd19
aplyd <austin@ftacnik.com>
2019-08-22 09:31:56 -0700

    add color sketch function

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let container = document.querySelector('.container');
 let clearBtn = document.querySelector('#clearBtn');
+let clearBtnColor = document.querySelector('#clearBtnColor');
 let pixel = document.querySelectorAll('.pixel');
 
 
-function createGrid(size) {
+
+function createGridNormal(size) {
     
     //erase previously created canvas https://stackoverflow.com/a/3955238
     while (container.firstChild) {
@@ -32,16 +34,59 @@ function createGrid(size) {
     }
 }
 
-createGrid(16);
+function createGridColor(size) {
+    
+    //erase previously created canvas https://stackoverflow.com/a/3955238
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    //
 
-function clear() {
+    for (x = 0; x < size; x++) {
+        for (y = 0; y < size; y++) {
+
+            //create and append divs
+            let div = document.createElement('div');
+            container.appendChild(div);
+            div.style.backgroundColor = 'lightgrey';
+            div.classList.add('pixel');
+
+            // update css variables to adjust grid size
+        
+            document.documentElement.style.setProperty('--size', size);
+            
+            let randomRedValue = Math.floor(Math.random() * 255) + 1;
+            let randomGreenValue = Math.floor(Math.random() * 255) + 1;
+            let randomBlueValue = Math.floor(Math.random() * 255) + 1;
+
+            div.addEventListener('mouseover', () => {
+                div.style.backgroundColor = "rgb(" + randomRedValue + "," + randomBlueValue + "," + randomGreenValue + ")";
+            })
+        }
+    }
+}
+
+createGridNormal(16);
+
+function clearNormal() {
     let size = Number(prompt('please enter a number between '));
         if (typeof size == 'number' && size > 0) {
-            createGrid(size);
+            createGridNormal(size);
         } else {
             alert('characters other than numbers are not supported');
                 clear();
             }
 }
 
-clearBtn.addEventListener('mouseup', clear);
+function clearColor() {
+    let size = Number(prompt('please enter a number between '));
+        if (typeof size == 'number' && size > 0) {
+            createGridColor(size);
+        } else {
+            alert('characters other than numbers are not supported');
+                clear();
+            }
+}
+
+clearBtn.addEventListener('mouseup', clearNormal);
+clearBtnColor.addEventListener('mouseup', clearColor);
