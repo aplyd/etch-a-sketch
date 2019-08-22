2a4a560ad9651c672d2bdb5fda93bca8c98284cc
aplyd <austin@ftacnik.com>
2019-08-22 11:15:22 -0700

    add greyscale sketch function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let container = document.querySelector('.container');
 let clearBtn = document.querySelector('#clearBtn');
 let clearBtnColor = document.querySelector('#clearBtnColor');
+let clearBtnGreyscale = document.querySelector('#clearBtnGreyscale');
 let pixel = document.querySelectorAll('.pixel');
 
 
@@ -66,6 +67,36 @@ function createGridColor(size) {
     }
 }
 
+function createGridGreyscale(size) {
+    
+    //erase previously created canvas https://stackoverflow.com/a/3955238
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    //
+
+    for (x = 0; x < size; x++) {
+        for (y = 0; y < size; y++) {
+
+            //create and append divs
+            let div = document.createElement('div');
+            container.appendChild(div);
+            div.style.backgroundColor = 'lightgrey';
+            div.classList.add('pixel');
+            div.style.opacity = "0.1";
+
+            // update css variables to adjust grid size
+        
+            document.documentElement.style.setProperty('--size', size);
+            
+
+            div.addEventListener('mouseover', () => {
+                console.log(div.style.opacity = Number(div.style.opacity) + 0.1);
+                div.style.backgroundColor = 'black';
+            })
+        }
+    }
+}
 
 createGridNormal(16);
 
@@ -75,7 +106,7 @@ function clearNormal() {
             createGridNormal(size);
         } else {
             alert('characters other than numbers are not supported');
-                clearNormal();
+                clear();
             }
 }
 
@@ -85,10 +116,20 @@ function clearColor() {
             createGridColor(size);
         } else {
             alert('characters other than numbers are not supported');
-                clearColor();
+                clear();
             }
 }
 
+function clearGreyScale() {
+    let size = Number(prompt('please enter a number between 12 & 240'));
+        if (typeof size == 'number' && size > 0) {
+            createGridGreyscale(size);
+        } else {
+            alert('characters other than numbers are not supported');
+                clear();
+            }
+}
 
 clearBtn.addEventListener('mouseup', clearNormal);
 clearBtnColor.addEventListener('mouseup', clearColor);
+clearBtnGreyscale.addEventListener('mouseup', clearGreyScale);
